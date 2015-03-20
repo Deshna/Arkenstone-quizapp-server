90452eab20e3360003c19c3f5b60962d08cec453
prateekchandan <prateekchandan5545@gmail.com>
2015-03-20 22:31:46 +0530

    ldap test

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -415,7 +415,8 @@ class APIController extends BaseController {
 
 		$ldap_id = Input::get('ldap_id');
 		$ldap_password = Input::get('ldap_password');
-		$ds = ldap_connect("ldap.iitb.ac.in") or return "Error";
+		$ds = ldap_connect("ldap.iitb.ac.in");
+		return $ds;
 		$sr = ldap_search($ds,"dc=iitb,dc=ac,dc=in","(uid=$ldap_id)");
 		$info = ldap_get_entries($ds, $sr);
 	    $roll = $info[0]["employeenumber"][0];
